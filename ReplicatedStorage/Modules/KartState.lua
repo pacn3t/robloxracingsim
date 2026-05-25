0839b7b948a1cd1fddec16bf4c6d2d4fc4d48293
pacnet <sultan511ahmed@gmail.com>
2026-05-25 19:20:23 +0800

    Create KartState.lua

---
+++ ReplicatedStorage/Modules/KartState.lua
@@ -0,0 +1,13 @@
+local KartState = {
+	body            = nil,
+	isInKart        = false,
+	currentSpeed    = 0,
+	currentLap      = 1,
+	totalLaps       = 3,
+	speedMultiplier = 1,
+	currentItem     = nil,
+	shieldActive    = false,
+	raceStarted     = false,  
+}
+
+return KartState
